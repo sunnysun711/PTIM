058bf011e92711fdbc653f94f9ecff1af27381a7
Yichen SUN AW51M <sunysun1996@outlook.com>
2025-04-03 19:17:10 +0800

    - Added src/globals.py for frequently used large data (K_PV_DICT, TT, AFC). - Added <find_feas_iti> method in src/passenger.py.

--- src/passenger.py
+++ src/passenger.py
@@ -2,33 +2,14 @@
 Please make sure path.pkl and pathvia.pkl are already generated.
 """
 import numpy as np
+
 from src.utils import read_data
-
-# get frequently used data: pathvia array
-PV = read_data("pathvia.pkl", show_timer=False).sort_values(["path_id"])
-# PV = read_data("pathvia.pkl", show_timer=False).sort_values(by=["path_id", "pv_id"])
-PV: np.ndarray = PV[PV["link_type"] == "in_vehicle"][["path_id", "node_id1", "node_id2", "line", "updown"]].values
-
-# get frequently used data: timetable array
-TT = read_data("TT", show_timer=False).reset_index().sort_values(["LINE_NID", "UPDOWN", "TRAIN_ID", "DEPARTURE_TS"])
-TT['ts1'] = TT["DEPARTURE_TS"] - TT["STOP_TIME"]  # make sure the carriage gate is open at ts1
-TT: np.ndarray[int] = TT[["TRAIN_ID", "STATION_NID", "LINE_NID", "UPDOWN", "ts1", "DEPARTURE_TS"]].values
+from src.globals import K_PV_DICT, TT, AFC
 
 
-def find_k_pv(_uid1: int, _uid2: int) -> np.ndarray:
+def find_trains(nid1: int, nid2: int, ts1: int, ts2: int, line: int, upd: int) -> list[tuple[int, int, int]]:
     """
-    Find pathvia array for k shortest paths.
-
-    :return: [path_id, node1, node2, line, updown]
-    """
-    base_path_id = _uid1 * 1000000 + _uid2 * 100 + 1
-    start_idx, end_idx = np.searchsorted(PV[:, 0], [base_path_id, base_path_id + 100])
-    return PV[start_idx:end_idx]
-
-
-def find_tt(nid1: int, nid2: int, ts1: int, ts2: int, line:int, upd: int) -> list[tuple[int, int, int]]:
-    """
-    Find timetable array for nid pairs within ts range.
+    Find trains, board_ts, alight_ts for nid pairs within ts range.
     Board_ts is obtained from departure_ts, alight_ts from arrive_ts.
 
     :return: [(train_id, board_ts, alight_ts), ...]
@@ -63,8 +44,94 @@ def find_tt(nid1: int, nid2: int, ts1: int, ts2: int, line:int, upd: int) -> lis
     return trains
 
 
+def find_feas_iti(k_pv, ts1, ts2):
+    """
 
+    :param k_pv:
+    :param ts1:
+    :param ts2:
+    :return:
+    """
+    # [[path_id, (train_id, b_ts, a_ts), (train_id, b_ts, a_ts), ...], [path_id, (train_id, b_ts, a_ts)], ...]
+    feas_iti_list: list[list[int | tuple[int, int, int]]] = []
+    for path_id in set(k_pv[:, 0]):
+        print(path_id)
+        pv = k_pv[k_pv[:, 0] == path_id]
+        # [[(train_id, b_ts, a_ts), (), ...], [], ...], len(seg_trains) = number of seg in this path
+        seg_trains: list[list[tuple[int, int, int]]] = []
+        ts1_this_seg = ts1
+        for seg_id, seg in enumerate(pv):
+            trains = find_trains(nid1=seg[1], nid2=seg[2], ts1=ts1_this_seg, ts2=ts2, line=seg[3], upd=seg[4])
+            seg_trains.append(trains)
+            if not trains:  # can not find trains, this path_id does not have feasible iti
+                break
+            ts1_this_seg = min([_[2] for _ in trains])
+        else:  # this path_id does have at least one feasible iti
+            # do a backward pruning step
+            ts2_this_seg = min([_[1] for _ in seg_trains[-1]])
+            for trains in reversed(seg_trains[:-1]):
+                trains[:] = [t for t in trains if t[2] < ts2_this_seg]  # prune
+                ts2_this_seg = min([_[1] for _ in trains])
+
+            # seg_trains ready. Now generate all feasible combinations
+            def backtrack(seg_idx: int, current_path: list[tuple[int, int, int]]):
+                """Try to build a complete feasible itinerary from seg_trains"""
+                if seg_idx == len(seg_trains):
+                    feas_iti_list.append([path_id] + current_path)
+                    return
+
+                for train in seg_trains[seg_idx]:
+                    if not current_path or current_path[-1][2] < train[1]:  # check transfer time > 0
+                        current_path.append(train)
+                        backtrack(seg_idx + 1, current_path)  # recursion
+                        current_path.pop()
+
+            backtrack(0, [])
+    return feas_iti_list
 
 
 if __name__ == '__main__':
+    # rid, uid1, ts1, uid2, ts2 = AFC[np.random.choice(len(AFC))]
+    rid, uid1, ts1, uid2, ts2 = AFC[AFC[:, 0] == 1170139].flatten()
+    print(rid, uid1, uid2, ts1, ts2)
+
+    k_pv = K_PV_DICT[(uid1, uid2)]
+    print(k_pv)
+
+    # [[path_id, (train_id, b_ts, a_ts), (train_id, b_ts, a_ts), ...], [path_id, (train_id, b_ts, a_ts)], ...]
+    feas_iti_list: list[list[int | tuple[int, int, int]]] = []
+    for path_id in set(k_pv[:, 0]):
+        print(path_id)
+        pv = k_pv[k_pv[:, 0] == path_id]
+        # [[(train_id, b_ts, a_ts), (), ...], [], ...], len(seg_trains) = number of seg in this path
+        seg_trains: list[list[tuple[int, int, int]]] = []
+        ts1_this_seg = ts1
+        for seg_id, seg in enumerate(pv):
+            trains = find_trains(nid1=seg[1], nid2=seg[2], ts1=ts1_this_seg, ts2=ts2, line=seg[3], upd=seg[4])
+            seg_trains.append(trains)
+            if not trains:  # can not find trains, this path_id does not have feasible iti
+                break
+            ts1_this_seg = min([_[2] for _ in trains])
+        else:  # this path_id does have at least one feasible iti
+            # do a backward pruning step
+            ts2_this_seg = min([_[1] for _ in seg_trains[-1]])
+            for trains in reversed(seg_trains[:-1]):
+                trains[:] = [t for t in trains if t[2] < ts2_this_seg]  # prune
+                ts2_this_seg = min([_[1] for _ in trains])
+
+            # seg_trains ready. Now generate all feasible combinations
+            def backtrack(seg_idx: int, current_path: list[tuple[int, int, int]]):
+                """Try to build a complete feasible itinerary from seg_trains"""
+                if seg_idx == len(seg_trains):
+                    feas_iti_list.append([path_id] + current_path)
+                    return
+
+                for train in seg_trains[seg_idx]:
+                    if not current_path or current_path[-1][2] < train[1]:  # check transfer time > 0
+                        current_path.append(train)
+                        backtrack(seg_idx + 1, current_path)  # recursion
+                        current_path.pop()
+
+            backtrack(0, [])
+    print(feas_iti_list)
     pass
