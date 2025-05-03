f92c5fcbd07aae70d64056847950ca4d0941e47f
Sunny PC <sunnysun1996@outlook.com>
2025-05-04 00:43:18 +0800

    Refactor: update penal_func to accept capacities as a tuple; add plot_penal_funcs for visualizing penalty functions; enhance test.ipynb with new outputs and data filtering

--- src/congest_penal.py
+++ src/congest_penal.py
@@ -1,6 +1,8 @@
+from typing import Callable
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.stats as st
 
 from src import config
 from src.utils import read_
@@ -11,16 +13,14 @@ from src.utils import read_
 # penalty = \Pi_{i=1}^n (penal_func(train_load_i))
 
 
-def penal_func(x: float | np.ndarray, cap: float, cap_max: float, type_: str = "x", k: float = None) -> float | np.ndarray:
+def penal_func(x: float | int | np.ndarray, capacities: tuple[float, float], type_: str = "x", k: float = None) -> float | np.ndarray:
     """
     Calculate the congestion penalty.
 
     :param x: The load of the train.
-    :type x: float | np.ndarray
-    :param cap: The capacity of the train.
-    :type cap: float
-    :param cap_max: The maximum capacity of the train.
-    :type cap_max: float
+    :type x: float | int | np.ndarray
+    :param capacities: The normal capacity and max capcity of the train.
+    :type capacities: tuple[float, float]
     :param type_: The type of the congestion penalty.
     :type type_: str, optional, default="x"
     :param k: The k value of the congestion penalty.
@@ -31,12 +31,83 @@ def penal_func(x: float | np.ndarray, cap: float, cap_max: float, type_: str = "
 
     Example:
     --------
-    >>> penal_func(1845, 1680.0, 1980.0, type_="x")
-    0.5
+    >>> penal_func(1845, (1680.0, 1980.0), type_="x")
+    0.45
     """
-    x = np.atleast_1d(x)
-    penal: np.ndarray = ...
+    cn, cc = capacities  # cap for normal, cap for commuter
+    x = np.atleast_1d(x).astype(float)
+    if type_ == "x":
+        penal = np.piecewise(
+            x,
+            [x <= cn, (x > cn) & (x < cc), x >= cc],
+            [1, lambda p: (cc - p) / (cc - cn), 0]
+        )
+    elif type_ == "x2":
+        penal = np.piecewise(
+            x,
+            [x <= cn, (x > cn) & (x < cc), x >= cc],
+            [1, lambda p: 1 - ((p - cn) / (cc - cn)) ** 2, 0]
+        )
+    elif type_ == "y2":
+        penal = np.piecewise(
+            x,
+            [x <= cn, (x > cn) & (x < cc), x >= cc],
+            [1, lambda p: ((p - cc) / (cn - cc)) ** 0.5, 0]
+        )
+    elif type_ == "normal":
+        penal = np.piecewise(
+            x,
+            [
+                x <= cn, (x > cn) & (x < cc), x >= cc
+            ],
+            [
+                1,
+                lambda p: (1 - st.norm.sf(k * (p * 2 - cc - cn) /
+                           (cn - cc)) - st.norm.sf(k)) / (1 - 2 * st.norm.sf(k)),
+                0
+            ]
+        )
+    else:
+        raise ValueError(f"Penalty type {type_} not supported!")
     return penal if penal.size > 1 else penal[0]
+
+
+def plot_penal_funcs():
+    import matplotlib
+    matplotlib.use('TkAgg')
+    
+    from matplotlib import pyplot as plt
+    
+    fig, ax = plt.subplots(1, 1, figsize=(6,4), facecolor="white")
+    cn, cc = 10, 20
+    x = np.linspace(cn-2, cc+2, num=1000)
+    p_x = penal_func(x, (cn, cc), type_="x")
+    p_x2 = penal_func(x, (cn, cc), type_="x2")
+    p_y2 = penal_func(x, (cn, cc), type_="y2")
+    p_normal_k1 = penal_func(x, (cn, cc), type_="normal", k=1)
+    p_normal_k3 = penal_func(x, (cn, cc), type_="normal", k=3)
+    p_normal_k5 = penal_func(x, (cn, cc), type_="normal", k=5)
+    
+    ax.plot(x, p_x, label="x")
+    ax.plot(x, p_x2, label="x2")
+    ax.plot(x, p_y2, label="y2")
+    ax.plot(x, p_normal_k1, label="normal k1")
+    ax.plot(x, p_normal_k3, label="normal k3")
+    ax.plot(x, p_normal_k5, label="normal k5")
+    
+    ax.set_xticks([cn, cc])
+    ax.set_xticklabels(["Normal", "Commuters"])
+    ax.set_xlabel("Train load / Standing passenger density")
+    
+    ax.set_ylabel("Penalty value")
+    
+    ax.legend(title="Penalty function types")
+    plt.show()
+    return
+    
+    
+    
+    
 
 
 def build_penalty_dict(overload_train_section: dict[int, np.ndarray]) -> dict[tuple[int, int, int], float]:
@@ -45,9 +116,9 @@ def build_penalty_dict(overload_train_section: dict[int, np.ndarray]) -> dict[tu
     from the overload train section dict.
 
     :param overload_train_section: The overload train section dict. {train_id: np.ndarray} 
-        
+
         columns are: ['sta1_nid', 'dep_ts','sta2_nid', 'arr_ts', 'load'] (only overload sections) 
-        
+
         should generated from `src.timetable.find_overload_train_section()`
     :type overload_train_section: dict[int, np.ndarray]
 
@@ -60,4 +131,6 @@ def build_penalty_dict(overload_train_section: dict[int, np.ndarray]) -> dict[tu
     return penalty_dict
 
 
-
+if __name__ == "__main__":
+    plot_penal_funcs()
+    pass
