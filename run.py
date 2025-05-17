f0240ff5918821fcf4b3b80f5c27bfbd8f4c2a63
Sunny PC <sunnysun1996@outlook.com>
2025-05-18 03:32:22 +0800

    [Publish]: Fix run.py; remove deprecated trajectory_2.py module.

--- run.py
+++ run.py
@@ -1,27 +1,49 @@
-def step1():
-    """
-    Step 1: Prepare the metro network structure for pathfinding.
-    """
-    from scripts.prep_network import main as prep_network_main
-    prep_network_main()
+import argparse
 
 
-def step2():
-    """
-    Step 2: Find feasible itineraries for each trajectory.
-    """
-    from scripts.find_feas_iti import main as find_feas_iti_main
-    find_feas_iti_main()
+def main(config_file):
+    # Importing the necessary modules
+    from src import config
 
+    # Load the configuration using the config file path
+    config.load_config(config_file)
 
-def step3():
-    """
-    Step 3:
-    """
-    pass
+    # Running the various modules
+    if not config.CONFIG["use_existing"]["network"]:
+        from scripts import prep_network
+        prep_network.main()
+
+    if not config.CONFIG["use_existing"]["itinerary"]:
+        from scripts import find_feas_iti
+        find_feas_iti.main()
+
+    if not config.CONFIG["use_existing"]["trajectory"]:
+        from scripts import split_feas_iti
+        split_feas_iti.main()
+
+    if not config.CONFIG["use_existing"]["walk_times"]:
+        from scripts import analyze_walk_time
+        analyze_walk_time.main()
+
+    from scripts import calculate_distribution
+    calculate_distribution.main()
+    
+    from scripts import assign_traj
+    assign_traj.main()
 
 
 if __name__ == "__main__":
-    step1()
-    step2()
-    pass
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(
+        description="Run the script with a given config file.")
+    parser.add_argument(
+        "--config", "-c",
+        type=str,
+        required=False,  # Optional argument to allow defaults
+        default="configs/config1.yaml",  # Default config path
+        help="Path to the configuration file."
+    )
+    args = parser.parse_args()
+
+    # Pass the config file to the main function
+    main(args.config)
